b8de8303396f6ce55ba5e387d7fd625abe638e0a
arbuzinside <arbuzinside@gmail.com>
2014-02-14 08:30:46 +0200

    Changes for Heroku

--- src/PhotoSite/settings.py
+++ src/PhotoSite/settings.py
@@ -10,12 +10,12 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-#TEMPLATE_DIRS = (
-#  BASE_DIR + '/PhotoAlbum/Templates/'
-#)
+TEMPLATE_DIRS = (
+  BASE_DIR + '/PhotoAlbum/Templates/'
+)
 
 
 # Quick-start development settings - unsuitable for production
@@ -94,7 +94,7 @@ SOCIAL_AUTH_FACEBOOK_SECRET = '40d43486cc9885c35db222d13a82e9ac'
 FACEBOOK_EXTENDED_PERMISSIONS = ['email']
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -105,13 +105,23 @@ DATABASES = {
         'PORT': '', 
     }
 }
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
+
+
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
@@ -129,8 +139,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-#STATICFILES_DIRS = ( '/home/dima/git/group-22-2013/src/PhotoAlbum/Static/',)
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/Static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '/PhotoAlbum/Static/'),
+)
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'moments.albums@gmail.com'
