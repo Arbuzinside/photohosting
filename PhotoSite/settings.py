a51bb07814ba056b54340d36c8a2561c18c1209f
arbuzinside <arbuzinside@gmail.com>
2014-02-14 10:48:37 +0200

    Database fix

--- PhotoSite/settings.py
+++ PhotoSite/settings.py
@@ -95,16 +95,16 @@ FACEBOOK_EXTENDED_PERMISSIONS = ['email']
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '', 
-    }
-}
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.postgresql_psycopg2',
+   #     'NAME': 'postgres',
+    #    'USER': 'postgres',
+     #   'PASSWORD': 'admin',
+      #  'HOST': 'localhost',
+       # 'PORT': '', 
+    #}
+#}
 
 
 
